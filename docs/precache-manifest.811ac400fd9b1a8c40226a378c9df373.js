62a6b3ee293bed5927c742e87df5aba9bf26477f
Gian22Franco <gian.lofeudo94@gmail.com>
2020-08-12 12:35:34 -1200

    docs agregado

---
+++ docs/precache-manifest.811ac400fd9b1a8c40226a378c9df373.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ac1ecf71bc117aa1b92ab40fd0c3c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d45e7bed81b3cad95f8",
+    "url": "/static/css/main.a2621879.chunk.css"
+  },
+  {
+    "revision": "d39d869f913fde1fd6e0",
+    "url": "/static/js/2.6efa8b5a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6efa8b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d45e7bed81b3cad95f8",
+    "url": "/static/js/main.b55c949a.chunk.js"
+  },
+  {
+    "revision": "8ea78b991d42a9f47132",
+    "url": "/static/js/runtime-main.3657cf33.js"
+  },
+  {
+    "revision": "d54db6fe21066025d5bb815cfe3d0efe",
+    "url": "/static/media/ai.d54db6fe.png"
+  },
+  {
+    "revision": "33349b5d3b13a97050fc3776375b6ff1",
+    "url": "/static/media/carilo.33349b5d.jpg"
+  },
+  {
+    "revision": "98e67746fcf98735c511bd20571d8ef9",
+    "url": "/static/media/compu.98e67746.png"
+  },
+  {
+    "revision": "1706c9f16bd08eb5e03f1df3e0a94a1c",
+    "url": "/static/media/figma.1706c9f1.png"
+  },
+  {
+    "revision": "cda326a5f966c8a8f82212c10b693c44",
+    "url": "/static/media/futsal.cda326a5.jpg"
+  },
+  {
+    "revision": "1696f7df3f9dc3cf2cce8ede7be1c4f2",
+    "url": "/static/media/git.1696f7df.png"
+  },
+  {
+    "revision": "3899f0b89674303f466bb54e892cb287",
+    "url": "/static/media/image20.3899f0b8.png"
+  },
+  {
+    "revision": "15a4866e67b5a9aac43ae3af08de6721",
+    "url": "/static/media/js.15a4866e.png"
+  },
+  {
+    "revision": "84d3c34faa35b79ef9bbcb6368430b36",
+    "url": "/static/media/libreta.84d3c34f.png"
+  },
+  {
+    "revision": "1c2fded5a71453423168a1f08c721c4f",
+    "url": "/static/media/perros.1c2fded5.jpg"
+  },
+  {
+    "revision": "2e69916036ebe6d79cc93c8078c2408f",
+    "url": "/static/media/ps.2e699160.png"
+  },
+  {
+    "revision": "a7212ed4c02ca28a98c310a947c21998",
+    "url": "/static/media/react.a7212ed4.png"
+  },
+  {
+    "revision": "3147871421ab17c1222ed94a02909b05",
+    "url": "/static/media/sass.31478714.png"
+  },
+  {
+    "revision": "100cb99c82f5272e56733687a920de57",
+    "url": "/static/media/xd.100cb99c.png"
+  }
+]);
